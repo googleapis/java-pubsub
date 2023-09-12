307019a23cad3208307a1a3afb5d507d6becb2f4
Kamal Aboul-Hosn <kamal.aboulhosn@gmail.com>
2023-09-12 07:15:55 -0400

    Revert changes to original async example

--- samples/snippets/src/main/java/pubsub/SubscribeAsyncExample.java
+++ samples/snippets/src/main/java/pubsub/SubscribeAsyncExample.java
@@ -26,133 +26,19 @@ import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
-import com.google.api.gax.core.FixedExecutorProvider;
-import com.google.api.gax.core.ExecutorProvider;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.lang.Thread;
-import com.google.api.gax.rpc.TransportChannelProvider;
-import com.google.cloud.pubsub.v1.SubscriptionAdminSettings;
-import org.threeten.bp.Duration;
-import com.google.api.gax.rpc.TransportChannel;
-import com.google.auth.Credentials;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.Map;
 
 public class SubscribeAsyncExample {
   public static void main(String... args) throws Exception {
     // TODO(developer): Replace these variables before running the sample.
-    String projectId = "ordering-keys-testing";
-    String subscriptionId = "threads-test";
+    String projectId = "your-project-id";
+    String subscriptionId = "your-subscription-id";
 
     subscribeAsyncExample(projectId, subscriptionId);
   }
 
-  private static class SinglePubSubChannelProvider implements TransportChannelProvider {
-    TransportChannel channel;
-    TransportChannelProvider channelProvider;
-
-    SinglePubSubChannelProvider(ExecutorProvider executorProvider) {
-  int MAX_INBOUND_MESSAGE_SIZE =
-      20 * 1024 * 1024; // 20MB API maximum message size.
-      int MAX_INBOUND_METADATA_SIZE =
-      4 * 1024 * 1024; // 4MB API maximum metadata size
-      channelProvider =
-        SubscriptionAdminSettings.defaultGrpcTransportProviderBuilder()
-            .setMaxInboundMessageSize(MAX_INBOUND_MESSAGE_SIZE)
-            .setMaxInboundMetadataSize(MAX_INBOUND_METADATA_SIZE)
-            .setKeepAliveTime(Duration.ofMinutes(5))
-            .setExecutor(executorProvider.getExecutor()).build();
-    }
-
-    @Override
-    public TransportChannel getTransportChannel() {
-      if (channel == null) {
-        try {
-          channel = channelProvider.getTransportChannel();
-        } catch (Exception e) {
-        }
-      }
-      return channel;
-    }
-
-    @Override
-    public boolean acceptsPoolSize() {
-      return channelProvider.acceptsPoolSize();
-    }
-
-    @Override
-    public String getTransportName() {
-      return channelProvider.getTransportName();
-    }
-
-    @Override
-    public boolean needsCredentials() {
-      return channelProvider.needsCredentials();
-    }
-
-    @Override
-    public boolean needsEndpoint() {
-      return channelProvider.needsEndpoint();
-    }
-
-    @Override
-    public boolean needsExecutor() {
-      return channelProvider.needsExecutor();
-    }
-
-    @Override
-    public boolean needsHeaders() {
-      return channelProvider.needsHeaders();
-    }
-
-    @Override
-    public boolean shouldAutoClose() {
-      return channelProvider.shouldAutoClose();
-    }
-
-    @Override
-    public TransportChannelProvider withCredentials(Credentials credentials) {
-      channelProvider = channelProvider.withCredentials(credentials);
-      return this;
-    }
-
-    @Override
-    public TransportChannelProvider withEndpoint(String endpoint) {
-      channelProvider = channelProvider.withEndpoint(endpoint);
-      return this;
-    }
-
-    @Override
-    public TransportChannelProvider withExecutor(Executor executor) {
-      channelProvider = channelProvider.withExecutor(executor);
-      return this;
-    }
-
-    @Override
-    public TransportChannelProvider withExecutor(ScheduledExecutorService executor) {
-      channelProvider = channelProvider.withExecutor(executor);
-      return this;
-    }
-
-    @Override
-    public TransportChannelProvider withHeaders(Map<String,String> headers) {
-      channelProvider = channelProvider.withHeaders(headers);
-      return this;
-    }
-
-    @Override
-    public TransportChannelProvider withPoolSize(int size) {
-      channelProvider = channelProvider.withPoolSize(size);
-      return this;
-    }
-  }
-
   public static void subscribeAsyncExample(String projectId, String subscriptionId) {
-    int subCount = 100;
+    ProjectSubscriptionName subscriptionName =
+        ProjectSubscriptionName.of(projectId, subscriptionId);
 
     // Instantiate an asynchronous message receiver.
     MessageReceiver receiver =
@@ -163,30 +49,17 @@ public class SubscribeAsyncExample {
           consumer.ack();
         };
 
-    FixedExecutorProvider executorProvider =
-          FixedExecutorProvider.create(new ScheduledThreadPoolExecutor(1));
-    SinglePubSubChannelProvider channelProvider = new SinglePubSubChannelProvider(executorProvider);
-
-    List<Subscriber> subscribers = new ArrayList<>();
-    for (int i = 0; i < subCount; ++i) {
-      ProjectSubscriptionName subscriptionName =
-        ProjectSubscriptionName.of(projectId, subscriptionId + i);
-      Subscriber subscriber = null;
-        subscriber = Subscriber.newBuilder(subscriptionName, receiver).setChannelProvider(channelProvider).setExecutorProvider(executorProvider).setSystemExecutorProvider(executorProvider).build();
-        // Start the subscriber.
-        subscriber.startAsync().awaitRunning();
-      subscribers.add(subscriber);
-    }
-    System.out.printf("Thread count: %d\n", Thread.activeCount());
-    System.out.println("Listening for messages.");
-    for (Subscriber subscriber : subscribers) {
-      try {
-        // Allow the subscriber to run for 30s unless an unrecoverable error occurs.
-        subscriber.awaitTerminated(300, TimeUnit.SECONDS);
-      } catch (TimeoutException timeoutException) {
+    Subscriber subscriber = null;
+    try {
+      subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
+      // Start the subscriber.
+      subscriber.startAsync().awaitRunning();
+      System.out.printf("Listening for messages on %s:\n", subscriptionName.toString());
+      // Allow the subscriber to run for 30s unless an unrecoverable error occurs.
+      subscriber.awaitTerminated(30, TimeUnit.SECONDS);
+    } catch (TimeoutException timeoutException) {
       // Shut down the subscriber after 30s. Stop receiving messages.
-        subscriber.stopAsync();
-      }
+      subscriber.stopAsync();
     }
   }
 }
