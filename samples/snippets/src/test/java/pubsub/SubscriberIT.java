73407bc9b502ab75ed54cd27835e1f38ecab9884
Mike Prieto <mikeprieto@google.com>
2024-10-08 05:15:55 +0000

    test: Import AfterClass class

--- samples/snippets/src/test/java/pubsub/SubscriberIT.java
+++ samples/snippets/src/test/java/pubsub/SubscriberIT.java
@@ -39,6 +39,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
