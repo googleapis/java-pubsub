a842af7f205377715be06d514303d48ed65381f5
Mike Prieto <mikeprieto@google.com>
2025-10-21 14:44:38 +0000

    chore: Deflake WaiterTest

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -48,7 +48,7 @@ set +e
 case ${JOB_TYPE} in
 test)
     echo "SUREFIRE_JVM_OPT: ${SUREFIRE_JVM_OPT}"
-    mvn test -B -ntp -Dclirr.skip=true -Denforcer.skip=true ${SUREFIRE_JVM_OPT}
+    mvn test -B -ntp -Dclirr.skip=true -Denforcer.skip=true ${SUREFIRE_JVM_OPT} -Dtest=WaiterTest
     RETURN_CODE=$?
     ;;
 lint)
